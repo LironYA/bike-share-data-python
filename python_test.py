84c05da1a9441eb5e1ec8d9fa1194b79b0b5e084
LironYA <33940316+LironYA@users.noreply.github.com>
2020-05-27 20:40:44 +0100

    Create python file fore testing for pull git

---
+++ python_test.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# create numpy array of letters a-j
+# run this script in anaconda3 CMD
+letter_array = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+print("Array:", letter_array)
+
+# get dtype of array
+print("Dtype:", letter_array.dtype)
+
+# get shape of array
+print("Shape:", letter_array.shape)
+
+# get size of array
+print("Size:", letter_array.size)
